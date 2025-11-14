8d8f1ed66b4b89ecfcb4274136e7285f655b7e62
lei-rivera <105135536+lei-rivera@users.noreply.github.com>
2025-11-14 14:42:05 -0600

    Fix icon color for form submission

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const sectionThreeHeight = sectionTwoHeight + sections[2].offsetHeight;
 const sectionFourHeight = sectionThreeHeight + sections[3].offsetHeight;
 const removeLoop = () => {
     for (var i = 0; i < heading.length; i++) {
-            heading[i].classList.remove("white");
+            heading[i].classList.remove("active");
         }
 }
 
@@ -16,24 +16,15 @@ const removeLoop = () => {
 window.onscroll = () => {
     if (window.scrollY < sectionOneHeight) {
         removeLoop();
-        heading[0].classList.add("white");
+        heading[0].classList.add("active");
     } else if (window.scrollY >= sectionOneHeight && window.scrollY < sectionTwoHeight) {
         removeLoop();
-        heading[1].classList.add("white");
+        heading[1].classList.add("active");
     } else if (window.scrollY >= sectionTwoHeight && window.scrollY < sectionThreeHeight) {
         removeLoop();
-        heading[2].classList.add("white");
+        heading[2].classList.add("active");
     } else if (window.scrollY >= sectionThreeHeight) {
         removeLoop();
-        heading[3].classList.add("white");
+        heading[3].classList.add("active");
     }
 }
-
-
-// MOBILE HEADER
-// if (window.innerHeight > 490) {
-//     if (document.body.scrollTop >= 50) {
-//         heading.classList.remove("white");
-//         heading[0].classList.add("white");
-//     }
-// }
--- old-files/main.js
+++ old-files/main.js
@@ -8,7 +8,7 @@ const sectionThreeHeight = sectionTwoHeight + sections[2].offsetHeight;
 const sectionFourHeight = sectionThreeHeight + sections[3].offsetHeight;
 const removeLoop = () => {
     for (var i = 0; i < heading.length; i++) {
-            heading[i].classList.remove("active");
+            heading[i].classList.remove("white");
         }
 }
 
@@ -16,15 +16,24 @@ const removeLoop = () => {
 window.onscroll = () => {
     if (window.scrollY < sectionOneHeight) {
         removeLoop();
-        heading[0].classList.add("active");
+        heading[0].classList.add("white");
     } else if (window.scrollY >= sectionOneHeight && window.scrollY < sectionTwoHeight) {
         removeLoop();
-        heading[1].classList.add("active");
+        heading[1].classList.add("white");
     } else if (window.scrollY >= sectionTwoHeight && window.scrollY < sectionThreeHeight) {
         removeLoop();
-        heading[2].classList.add("active");
+        heading[2].classList.add("white");
     } else if (window.scrollY >= sectionThreeHeight) {
         removeLoop();
-        heading[3].classList.add("active");
+        heading[3].classList.add("white");
     }
 }
+
+
+// MOBILE HEADER
+// if (window.innerHeight > 490) {
+//     if (document.body.scrollTop >= 50) {
+//         heading.classList.remove("white");
+//         heading[0].classList.add("white");
+//     }
+// }
